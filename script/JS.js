e844da3864fdf5ef639cdabc3001d1d890385459
TMaenhout <t.maenhoutt@gmail.com>
2024-06-17 09:51:09 +0200

    Add files via upload

---
+++ script/JS.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    const offset = -slideIndex * 100;
+    document.querySelector('.slider-container').style.transform = translateX(${offset}%);
+}
+
+function prevSlide() {
+    showSlide(--slideIndex);
+}
+
+function nextSlide() {
+    showSlide(++slideIndex);
+}
+
+setInterval(nextSlide, 3000);
